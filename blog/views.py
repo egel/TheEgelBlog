74622287aab50af2ea4e91cdf210a6f698dfb915
Maciej Sypien <maciejsypien@gmail.com>
2014-03-24 01:09:50 +0100

    Improved views and adjust for models

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,30 @@
-from blog.models import Post
-from django.shortcuts import render_to_response
+from blog.models import Post, Category
+from django.shortcuts import render_to_response, get_object_or_404
 
-def tagpage(request, tag):
-  posts = Post.objects.filter(tags__name=tag)
-  return render_to_response("tagpage.html", {"posts":posts, "tag":tag})
+def index(request):
+  return render_to_response('blog.html', {
+    'categories': Category.objects.all(),
+    'posts': Post.objects.all()[:5],
+  })
+
+def view_post(request, slug):
+  return render_to_response('post.html', {
+    'post': get_object_or_404(Post, slug=slug),
+  })
+
+def view_category(request, slug):
+  category = get_object_or_404(Category, slug=slug)
+  return render_to_response('category.html', {
+    'category': category,
+    'posts': Post.objects.filter(categories = category)[:5],
+  })
+
+def view_categories(request):
+  return render_to_response('categories.html', {
+    'categories': Category.objects.all()
+  })
+
+def view_archives(request):
+  return render_to_response('archives.html', {
+    'posts': Post.objects.all().order_by("-created"),
+  })
