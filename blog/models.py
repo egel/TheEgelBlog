c8a3badb49f4b69c13e01cbe78e67fdced7b9465
Maciej Sypien <maciejsypien@gmail.com>
2014-03-24 01:03:27 +0100

    Improve models with categories; Removed taggit

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from taggit.managers import TaggableManager
 
+
 class Post(models.Model):
-  title = models.CharField(max_length=100)
+  title = models.CharField(max_length=140, unique=True)
+  slug = models.SlugField(max_length=140, unique=True)
   body = models.TextField()
   created = models.DateTimeField()
-  tags = TaggableManager()
+  categories = models.ManyToManyField('blog.Category')
 
   def __unicode__(self):
     return self.title
+
+  @models.permalink
+  def get_absolute_url(self):
+    return ('view_blog_post', None, { 'slug': self.slug })
+
+
+class Category(models.Model):
+  name = models.CharField(max_length=100, db_index=True)
+  slug = models.SlugField(max_length=100, db_index=True)
+
+  def __unicode__(self):
+    return self.name
+
+  @models.permalink
+  def get_absolute_url(self):
+    return ('view_blog_category', None, { 'slug': self.slug })
