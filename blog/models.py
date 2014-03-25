221822aa4fd914c31604bf09ad2369bf2e5661e4
Maciej Sypien <maciejsypien@gmail.com>
2014-03-25 20:43:31 +0100

    Improved with docstrings

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from tinymce.models import HTMLField
 
 
 class Post(models.Model):
+    """Model for single post in blog"""
     title = models.CharField(max_length=140, unique=True)
     slug = models.SlugField(max_length=140, unique=True)
     body = HTMLField()
@@ -16,10 +17,12 @@ class Post(models.Model):
 
     @models.permalink
     def get_absolute_url(self):
+        """Return absolute path using 'slug' for single post"""
         return ('view_blog_post', None, {'slug': self.slug})
 
 
 class Category(models.Model):
+    """Category model for grouping post"""
     name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
 
@@ -28,10 +31,12 @@ class Category(models.Model):
 
     @models.permalink
     def get_absolute_url(self):
+        """Return absolute path using 'slug' for single category"""
         return ('view_blog_category', None, {'slug': self.slug})
 
 
 class Tag(models.Model):
+    """Category model for grouping post"""
     name = models.CharField(max_length=50, db_index=True)
     slug = models.SlugField(max_length=50, db_index=True)
 
@@ -40,4 +45,5 @@ class Tag(models.Model):
 
     @models.permalink
     def get_absolute_url(self):
+        """Return absolute path using 'slug' for single tag"""
         return ('view_blog_tags', None, {'slug': self.slug})
